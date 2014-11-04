5e3269bb99b6a2a79594cdf120bb25d68d1a346f
YuyaItoh <y-itoh@ai.soc.i.kyoto-u.ac.jp>
2014-11-05 01:36:05 +0900

    friendships/listの更新
    最新のメッセージを取得できるようにした．更新順にソートできるようにした

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,7 +2,36 @@ class FriendshipsController < ApplicationController
     # GET friendships/list
     # params: user_id
     # 表示可能な異性(1度でも出会ったことのある異性)を検索
+    # メッセージが新しい人程上にくるように設計したかったからぐちゃぐちゃ
     def list
-        @friends = Friendship.where(source_id: params[:user_id]).order(updated_at: :desc)
+        friends = Friendship.where(source_id: params[:user_id]).order(updated_at: :desc)
+        @res = []
+        friends.each do |friend|
+            # friendと自分の最後の会話を取得
+            group = [friend.source_id, friend.target_id]
+            m = Message.where(source_id: group, target_id: group, deleted: false).order(id: :desc).first
+
+            # メッセージがなければテンプレートを用意
+            if m.nil? then
+                date = friend.updated_at.strftime("%m/%d")
+                msg = date + "に出会いました"
+                updated_at = friend.updated_at
+            else
+                msg = m.message
+                updated_at = (friend.updated_at > m.updated_at) ? friend.updated_at : m.updated_at
+            end
+
+            f =  {
+                user_id: friend.target_id,
+                user_name: friend.target.username,
+                first_time: friend.first_time,
+                prev_time: updated_at, 
+                last_message: msg
+                }
+            f[:thumb] = friend.first_time? ? friend.target.image(:mosaic) : friend.target.image(:thumb)
+            @res.push(f)
+        end
+        @res = @res.sort_by{|val| val[:prev_time]}.reverse
+
     end
 end
--- app/views/friendships/list.json.jbuilder
+++ app/views/friendships/list.json.jbuilder
@@ -1,12 +1,8 @@
-json.array!(@friends) do |friend|
-    json.user_id friend.target.id
-    json.user_name friend.target.username
-    if friend.first_time? then
-        json.thumb friend.target.image(:mosaic)
-    else
-        json.thumb friend.target.image(:thumb)
-    end
-    json.first_time friend.first_time
-    json.prev_cafe_name friend.cafename
-    json.prev_time friend.updated_at
+json.array!(@res) do |friend|
+    json.user_id friend[:user_id]
+    json.user_name friend[:user_name]
+    json.thumb friend[:thumb]
+    json.first_time friend[:first_time]
+    json.updated_at friend[:prev_time]
+    json.latest_message friend[:last_message]
 end
